85839699ee6aa9b2ef0a5677bccb8ebd01aa7691
Wilmer <wiland2315@gmail.com>
2019-03-30 16:28:49 -0500

    Tags added to image

--- events-api-axios/src/components/Event.js
+++ events-api-axios/src/components/Event.js
@@ -5,15 +5,19 @@ const Event = props => {
 	const {logo, url, name, start, summary, is_free, description, currency} = props.event;
 	const imageUrl = logo && logo.url ? logo.url : "http://www.vacationmexicobeach.com/images/no-image-available2.jpg";
 	const cardText = description && description.text ? description.text.substring(0, 100) + "..." : summary;
-
+	
   return (
     <div className="col-md-6">
 			<div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
 				<div className="card">
-					<img src={imageUrl} className="card-img-top img-noticias img-fluid" alt={name.text} />
+					<div className="card-img-container">
+						<img src={imageUrl} className="card-img-top img-noticias img-fluid" alt={name.text} />
+						<div className="d-flex justify-content-flex-start card-tags">
+							<span className="badge badge-success card-badge">{is_free ? "Gratis" : "Pago"}</span>
+							<span className="badge badge-warning card-badge">{currency}</span>
+						</div>
+					</div>
 					<small className="text-muted ml-2">{start.timezone} - {start.local}</small>
-					<span className="badge badge-success card-badge">{is_free ? "Gratis" : "Pago"}</span>
-					{currency && <span className="badge badge-success card-badge">{currency}</span>}
 					<div className="card-body">
 						<h5 className="card-title">{name.text}</h5>
 						<p className="card-text">{cardText}</p>
